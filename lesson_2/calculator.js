8018631cf34f76a55d4e44f5abcd02e8368bceae
Mustafa Ilikkan <mustafa.ilikkan@ozguryazilim.com.tr>
2024-02-06 22:37:12 +0300

    seperate messages into a config file

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -1,7 +1,12 @@
 const readline = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json');
 
 function prompt(msg) {
   console.log(`=> ${msg}`);
+}
+
+function seperator() {
+  console.log('-------------------------------');
 }
 
 function invalidNumber(num) {
@@ -46,27 +51,28 @@ function calculate(number1, number2, operation) {
   return result;
 }
 
-prompt('Welcome to the Calculator!');
+prompt(MESSAGES['welcome']);
 
 while (true) {
+  seperator();
   // get first number
   let number1 = getInput(
-    'What is the first number?',
-    "Hmmm...that doesn't look like a valid number.",
+    MESSAGES['firstNumber'],
+    MESSAGES['invalidNumber'],
     invalidNumber
   );
 
   // get second number
   let number2 = getInput(
-    'What is the second number?',
-    "Hmmm...that doesn't look like a valid number.",
+    MESSAGES['secondNumber'],
+    MESSAGES['invalidNumber'],
     invalidNumber
   );
 
   // get the operation
   let operation = getInput(
-    'What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide',
-    'Must choose 1, 2, 3, or 4.',
+    MESSAGES['chooseOperation'],
+    MESSAGES['invalidOperation'],
     invalidOperation
   );
 
@@ -74,16 +80,17 @@ while (true) {
   let output = calculate(number1, number2, operation);
 
   // display the result of the operation
-  prompt(`The result is ${output}.`);
+  prompt(`${MESSAGES['result']} ${output}.`);
+  seperator();
 
   // ask for another calculation
   let another = getInput(
-    'Would you like to perform another operation? (y/n)',
-    "Please enter either 'Yes' or 'No'...",
+    MESSAGES['anotherCalculation'],
+    MESSAGES['invalidAnotherCalculation'],
     invalidAnotherCalculation
   );
 
   if (another[0].toLowerCase() !== 'y') break;
 }
-
-prompt('Goodbye! Thanks for using calculator...');
+seperator();
+prompt(MESSAGES['goodbye']);
